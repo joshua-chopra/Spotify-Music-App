2ad79bd5d6b2deb619c2ab76902fb9baccd4649c
joshua-chopra <jchopra@seas.upenn.edu>
2021-03-20 16:29:09 -0400

    register views to urls

--- music_controller/frontend/urls.py
+++ music_controller/frontend/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import index
 
 urlpatterns = [
-    # register index function (which will render index.html template) to '' route. 
+    # register index function (which will render index.html template) to all routes.
     path('', index),
     path('join', index),
     path('create', index)
